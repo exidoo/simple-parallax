f115dfc5c415f9bac3ca23aa7de5c95a3a08cf8f
Romi RA <129487606+exidoo@users.noreply.github.com>
2023-04-08 20:05:39 +0700

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,39 @@
+const typed = new Typed('.intro', {
+  strings: ['Hello...👋', 'How Are U?'],
+  typeSpeed: 100,
+  backSpeed: 100,
+  loop: true,
+  loopCount: Infinity,
+});
+
+const astro = new Typed('.astro', {
+  strings: ['Im ....', 'An Astrophile'],
+  typeSpeed: 100,
+  backSpeed: 100,
+  loop: true,
+  loopCount: Infinity,
+});
+
+const name = new Typed('.name', {
+  strings: ['Romi Ramdani Lajuardi'],
+  typeSpeed: 150,
+  backSpeed: 150,
+  loop: true,
+  loopCount: Infinity,
+});
+
+const nycto = new Typed('.nycto', {
+  strings: ['and ...', 'A Nyctophile'],
+  typeSpeed: 100,
+  backSpeed: 100,
+  loop: true,
+  loopCount: Infinity,
+});
+
+const thanks = new Typed('.thanks', {
+  strings: ['Thanks for coming 👋'],
+  typeSpeed: 100,
+  backSpeed: 100,
+  loop: true,
+  loopCount: Infinity,
+});
